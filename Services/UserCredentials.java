29066cf136f03e47e4633745bdefd77edf6e1b2f
johnsonj20 <64604755+johnsonj20@users.noreply.github.com>
2020-06-12 16:37:03 -0700

    Update UserCredentials.java
    updated username

--- Services/UserCredentials.java
+++ Services/UserCredentials.java
@@ -1,7 +1,7 @@
 package Services;
 
 public class UserCredentials {
-        String verifiedUserName = "saic_user";
+        String verifiedUserName = "saic_user01";
         String verifiedPassword = "password";
         String verifiedPasswordOTP = "password123";
     }
